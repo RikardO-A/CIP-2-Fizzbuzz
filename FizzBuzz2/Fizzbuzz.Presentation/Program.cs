c2eb3054ffa2787d63c3f24ad7b588bb534ddae0
Rikard O.A <rikard.okello-akanga@sveaskog.se>
2025-09-16 08:31:46 +0200

    Null reference fix

--- FizzBuzz2/Fizzbuzz.Presentation/Program.cs
+++ FizzBuzz2/Fizzbuzz.Presentation/Program.cs
@@ -3,7 +3,7 @@ using FizzBuzz.Application;
 using FizzBuzz.Domain;
 
 Console.WriteLine("Enter an integer (Between 1 to 10^15)");
-long input = long.Parse(Console.ReadLine());
+long input = long.Parse(Console.ReadLine() ?? "CS8604 Null reference detta tar bort erroret xD");
 if (input < 1 || input > 1000000000000000)
 {
     Console.WriteLine("Input must be between 1 and 10^15");
